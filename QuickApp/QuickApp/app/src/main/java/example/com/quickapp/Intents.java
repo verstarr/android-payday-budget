dca97452eff755dc23fe15cb04153b7bbcf6442b
BIGCR <xr100chris@gmail.com>
2015-06-08 14:44:43 -0500

    Multi-Fragment Activity

---
+++ QuickApp/QuickApp/app/src/main/java/example/com/quickapp/Intents.java
@@ -0,0 +1,17 @@
+package example.com.quickapp;
+
+/**
+ * Created by CXR0963 on 6/6/2015.
+ */
+public class Intents {
+    public static final class Calendar {
+        /**
+         *
+         */
+        public static final String ACTION = "Calendar.intent.action.Launch";
+
+        private Calendar() {
+            // This class is not instantiable.
+        }
+    }
+}
