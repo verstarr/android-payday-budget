5395046f3cc137e75fc494046e1e3ecf6c41b937
Ver Starr <ver.starr@utexas.edu>
2015-06-08 19:23:05 -0500

    Changed main activity layout to fragment views. Need calendar fragment then we can start on logic

--- Days2PayDay/app/src/main/java/net/verstarr/days2payday/BudgetListFragment.java
+++ Days2PayDay/app/src/main/java/net/verstarr/days2payday/BudgetListFragment.java
@@ -1,11 +1,35 @@
 package net.verstarr.days2payday;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+
 
 /**
  * Created by VerStarr22 on 6/6/15.
  */
 public class BudgetListFragment extends Fragment {
     // budgetList logic
+
+    private static final String BUDGETS = "budgets";
+
+    private SharedPreferences savedBudgets; // user's favorite searches
+    private ArrayList<String> tags; // list of tags for saved searches
+    private ArrayAdapter<String> adapter; // binds tags to ListView
+
+    // configures the QuizFragment when its View is created
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view =
+                inflater.inflate(R.layout.fragment_budget_list, container, false);
+
+        return view;
+    }
 
 }
---
+++ Days2PayDay/app/src/main/java/net/verstarr/days2payday/PaydayBannerFragment.java
@@ -0,0 +1,9 @@
+package net.verstarr.days2payday;
+
+import android.app.Fragment;
+
+/**
+ * Created by VerStarr22 on 6/8/15.
+ */
+public class PaydayBannerFragment extends Fragment {
+}
