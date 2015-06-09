3b8f08205ea03b4d6407a9070a825adad790a1db
BIGCR <xr100chris@gmail.com>
2015-06-09 14:38:12 -0500

    Expand/Collapse Calendar function working

--- QuickApp/app/src/main/java/example/com/quickapp/SecondaryActivityView.java
+++ QuickApp/app/src/main/java/example/com/quickapp/SecondaryActivityView.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import net.verstarr.days2payday.BudgetListFragment;
 import net.verstarr.days2payday.PayDayBannerFragment;
@@ -16,11 +17,15 @@ public class SecondaryActivityView extends ActionBarActivity {
     private Fragment listViewFrag;
     private FragmentTransaction fragTransaction;
     private FragmentTransaction listFragTransaction;
+    private Button calendarButton;
+    MenuItem calendarExpandBtn;
+    MenuItem calendarMinimizeBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary_activity_view);
 
+        calendarButton = (Button)findViewById(R.id.expand_calendar);
         //TODO add if SavedInstantState == null
 
         bannerFrag = new PayDayBannerFragment();
@@ -38,6 +43,10 @@ public class SecondaryActivityView extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_secondary_activity_view, menu);
+
+        calendarExpandBtn = menu.findItem(R.id.expand_calendar);
+        calendarMinimizeBtn = menu.findItem(R.id.minimize_calendar);
+
         return true;
     }
 
@@ -50,9 +59,32 @@ public class SecondaryActivityView extends ActionBarActivity {
 
         switch(id){
             case R.id.expand_calendar:
+
                 Fragment frag = new CalendarActivityFragment();
                 FragmentTransaction fragTrans = getFragmentManager().beginTransaction().replace(R.id.calendar_container, frag);
                 fragTrans.commit();
+
+                if(item.isVisible()) {
+                    calendarExpandBtn.setVisible(false);
+                    calendarMinimizeBtn.setVisible(true);
+                }
+                else{
+                    calendarMinimizeBtn.setVisible(true);
+                }
+                break;
+            case R.id.minimize_calendar:
+
+                Fragment tempfrag = new PayDayBannerFragment();
+                FragmentTransaction tempfragTrans = getFragmentManager().beginTransaction().replace(R.id.calendar_container, tempfrag);
+                tempfragTrans.commit();
+
+                if(item.isVisible()) {
+                    calendarMinimizeBtn.setVisible(false);
+                    calendarExpandBtn.setVisible(true);
+                }
+                else{
+                    calendarMinimizeBtn.setVisible(true);
+                }
                 break;
             default:
                 break;
