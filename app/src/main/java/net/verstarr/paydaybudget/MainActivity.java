9f31689f83ea88626515bf1160e49f62ce68fb34
Ver Starr <ver.starr@utexas.edu>
2015-06-13 17:04:17 -0500

    Added action bar to MainActivity as well

--- app/src/main/java/net/verstarr/paydaybudget/MainActivity.java
+++ app/src/main/java/net/verstarr/paydaybudget/MainActivity.java
@@ -3,13 +3,14 @@ package net.verstarr.paydaybudget;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Button tempButton;
     @Override
